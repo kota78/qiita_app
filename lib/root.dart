ef6f08134b67d6c77547c18ba7ac5a8e14196e58
ko_cha78 <kota1725@gmail.com>
2021-07-09 23:15:44 +0900

    ・トップページ ・下タブで画面切り替え ・設定画面のレイアウト

---
+++ lib/root.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:qiita_app/feed_page_02.dart';
+import 'package:qiita_app/tag_page_03.dart';
+import 'package:qiita_app/my_page_05.dart';
+import 'package:qiita_app/setting_page_06.dart';
+
+class Root extends StatefulWidget {
+  const Root({Key key}) : super(key: key);
+
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  int _selectedIndex = 0;
+  List<Widget> _widgetOptions = <Widget>[
+    FeedPage(),
+    TagPage(),
+    MyPage(),
+    SettingPage(),
+  ];
+
+  void _onItemTap(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Qiita'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt((_selectedIndex)),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "フィード"),
+          BottomNavigationBarItem(icon: Icon(Icons.camera),label: "タグ"),
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "マイページ"),
+          BottomNavigationBarItem(icon: Icon(Icons.camera),label: "設定"),
+      ],
+      currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+      ),
+    );
+  }
+}
